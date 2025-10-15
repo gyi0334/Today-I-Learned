85543312340cc500cfee1e56d6804fd5ebda6547
지옥혈검 <gyi0334@naver.com>
2025-10-15 23:10:17 +0900

    Update 1015.py

--- codingtest/2025/1015.py
+++ codingtest/2025/1015.py
@@ -26,4 +26,48 @@ print('wrong sample : %d' % (y_test != y_pred).sum())
 print("accuracy: %.3f" % accuracy_score(y_test, y_pred))
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
-    
+    markers = ('s','x','o','^','v')
+    colors = ('red','blue','lightgreen','gray','cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])  # y의 class lable의 갯수만큼 color map 을 만든다
+
+    x1_min, x1_max = X[:,0].min() -1, X[:,0].max() +1
+    x2_min, x2_max = X[:,1].min() -1, X[:,1].max() +1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    print('origin Z shape : ',Z.shape)  # 확인용
+    Z = Z.reshape(xx1.shape)
+    print('reshaped Z shape : ',Z.shape)    # 확인용
+
+    # x.y 좌표에 대한 z값의 등고선을 색상으로 표현해서 등고선도 그리기
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap) 
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # 산점도 그리기 (x, y, size, color, alpha(마커의 투명도))
+    # x = X에서 y==cl 인 행만 선택하고, 0번째 열을 x값으로 사용(꽃잎 길이)
+    # y = X에서 y==cl 인 행만 선택하고, 1번째 열을 x값으로 사용(꽃잎 너비)
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],\
+                    alpha=0.8, c=colors[idx],
+                    marker=markers[idx], label=cl,
+                    edgecolor='black')
+        
+        if test_idx:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+            plt.scatter(X_test[:, 0], X_test[:, 1],
+                        facecolors='none', edgecolor='black', alpha=1.0,
+                        linewidth = 1, marker='o', s=100, label='test set')
+            
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std,
+                      y=y_combined,
+                      classifier=ppn,
+                      test_idx=range(105,150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
