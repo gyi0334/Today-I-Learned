5ca47065e9ced578e3c95690243401f0c8023e82
지옥혈검 <gyi0334@naver.com>
2025-10-23 23:05:01 +0900

    Update 1023.py

--- codingtest/2025/1023.py
+++ codingtest/2025/1023.py
@@ -17,7 +17,7 @@ df_wine.head()
 # 70 : 30 비율로 training dataset 과 test dataset으로 나누고 표준화 적용
 from sklearn.model_selection import train_test_split
 X, y= df_wine.iloc[:,1:].values, df_wine.iloc[:,0].values
-X_train, X_test, y_train, t_test = train_test_split(X, y, test_size=0.3, 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, 
                                                     stratify=y,
                                                     random_state=0)
 
@@ -36,6 +36,8 @@ eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 
 
 import matplotlib.pyplot as plt
+
+"""
 tot = sum(eigen_vals)
 var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
 cum_var_exp = np.cumsum(var_exp)
@@ -50,7 +52,7 @@ plt.legend(loc='best')
 plt.tight_layout()
 # plt.savefig('images/05_02.png', dpi=300)
 plt.show()
-
+"""
 
 # (고윳값, 고유벡터) 튜플의 리스트를 만듭니다
 eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
@@ -62,8 +64,97 @@ eigen_pairs.sort(key=lambda k: k[0], reverse=True)
 w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
                eigen_pairs[1][1][:, np.newaxis]))
 print('투영 행렬 W:\n', w)
+"""
+X_train_pca = X_train_std.dot(w)
+colors = ['r', 'b', 'g']
+markers = ['s', 'x', 'o']
 
+for l, c, m in zip(np.unique(y_train), colors, markers):
+    plt.scatter(X_train_pca[y_train == l, 0], 
+                X_train_pca[y_train == l, 1], 
+                c=c, label=l, marker=m)
 
-print(X_train_std.shape)
-print(w.shape)
-print((X_train_std.dot(w)).shape)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+# plt.savefig('images/05_03.png', dpi=300)
+plt.show()
+"""
+
+from sklearn.decomposition import PCA
+
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+
+plt.bar(range(1, 14), pca.explained_variance_ratio_, alpha=0.5, align='center')
+plt.step(range(1, 14), np.cumsum(pca.explained_variance_ratio_), where='mid')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+
+plt.show()
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # 마커와 컬러맵을 준비합니다
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 결정 경계를 그립니다
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # 클래스별로 샘플을 그립니다
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.6, 
+                    color=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx], 
+                    label=cl)
+        
+from sklearn.linear_model import LogisticRegression
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+lr = LogisticRegression(random_state=1)
+lr = lr.fit(X_train_pca, y_train)
+
+plot_decision_regions(X_train_pca, y_train, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+# plt.savefig('images/05_04.png', dpi=300)
+plt.show()
+
+plot_decision_regions(X_test_pca, y_test, classifier=lr)
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.legend(loc='lower left')
+plt.tight_layout()
+# plt.savefig('images/05_05.png', dpi=300)
+plt.show()
