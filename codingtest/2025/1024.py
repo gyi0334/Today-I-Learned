3d7860ac558e0d50bc0e80314ad9374c1d7a0ee3
지옥혈검 <gyi0334@naver.com>
2025-10-24 22:17:57 +0900

    Create 1024.py

---
+++ codingtest/2025/1024.py
@@ -0,0 +1,106 @@
+import pandas as pd
+
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                      'machine-learning-databases/wine/wine.data',
+                      header=None)
+
+# 70 : 30 비율로 training dataset 과 test dataset으로 나누고 표준화 적용
+from sklearn.model_selection import train_test_split
+X, y= df_wine.iloc[:,1:].values, df_wine.iloc[:,0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, 
+                                                    stratify=y,
+                                                    random_state=0)
+
+# 특성을 표준화처리
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# 고유벡터와 고윳값 쌍을 계산
+import numpy as np
+cov_mat = np.cov(X_train_std.T)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+#print('\n고윳값 \n%s' % eigen_vals)
+#print('\n고유벡터 \n%s' % eigen_vecs)
+
+
+import matplotlib.pyplot as plt
+
+# (고윳값, 고유벡터) 튜플의 리스트를 만듭니다
+eigen_pairs = [(np.abs(eigen_vals[i]), eigen_vecs[:, i])
+               for i in range(len(eigen_vals))]
+
+# 높은 값에서 낮은 값으로 (고윳값, 고유벡터) 튜플을 정렬합니다
+eigen_pairs.sort(key=lambda k: k[0], reverse=True)
+
+w = np.hstack((eigen_pairs[0][1][:, np.newaxis],
+               eigen_pairs[1][1][:, np.newaxis]))
+print('투영 행렬 W:\n', w)
+
+from sklearn.decomposition import PCA
+
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train_std)
+pca.explained_variance_ratio_
+
+plt.bar(range(1, 14), pca.explained_variance_ratio_, alpha=0.5, align='center')
+plt.step(range(1, 14), np.cumsum(pca.explained_variance_ratio_), where='mid')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+
+plt.show()
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.show()
+
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # 마커와 컬러맵을 준비합니다
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # 결정 경계를 그립니다
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # 클래스별로 샘플을 그립니다
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.6, 
+                    color=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx], 
+                    label=cl)
+        
+from sklearn.linear_model import LogisticRegression
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train_std)
+X_test_pca = pca.transform(X_test_std)
+
+lr = LogisticRegression(random_state=1)
+lr = lr.fit(X_train_pca, y_train)
+
+np.set_printoptions(precision=4)
+mean_vecs = []
+for label in range(1,4):
+    mean_vecs.append(np.mean(X_train_std[y_train==label], axis=0))
+    print('MV %s: %s\n' % (label, mean_vecs[label - 1]))
