434709eccca8a55db92a91aae5e4cb50f4b832d7
지옥혈검 <gyi0334@naver.com>
2025-10-30 00:06:39 +0900

    Update 1029.py

--- codingtest/2025/1029.py
+++ codingtest/2025/1029.py
@@ -4,7 +4,8 @@ df = pd.read_csv('https://archive.ics.uci.edu/ml/'
                  'machine-learning-databases'
                  '/breast-cancer-wisconsin/wdbc.data',
                  header=None)
-print(df.head())
+# print(df.head())
+# print('df.shape : ',df.shape)
 
 from sklearn.preprocessing import LabelEncoder
 
@@ -13,3 +14,18 @@ y = df.loc[:,1].values
 le = LabelEncoder()
 y = le.fit_transform(y)
 le.classes_
+print(le.transform(['M', 'B']))
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=1)
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+pipe_lr = make_pipeline(StandardScaler(), PCA(n_components=2),
+                        LogisticRegression(random_state=1))
+pipe_lr.fit(X_train, y_train)
+y_pred = pipe_lr.predict(X_test)
+print('test accuracy : %.3f' % pipe_lr.score(X_test, y_test))
