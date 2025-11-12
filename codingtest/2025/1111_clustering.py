0f5e422504616d155daff447d32ed0607fcf330a
지옥혈검 <gyi0334@naver.com>
2025-11-12 22:28:55 +0900

    Update 1111_clustering.py

--- codingtest/2025/1111_clustering.py
+++ codingtest/2025/1111_clustering.py
@@ -30,3 +30,56 @@ row_dist = pd.DataFrame(squareform(pdist(df, metric='euclidean')),
                         columns=labels,
                         index=labels)
 print(row_dist)
+
+from scipy.cluster.hierarchy import linkage
+
+row_clusters = linkage(pdist(df, metric='euclidean'), method='complete')
+pd.DataFrame(row_clusters,
+             columns=['row label 1', 'row label 2',
+                      'distance', 'no. of items in clust.'],
+             index=['cluster %d' % (i + 1)
+                    for i in range(row_clusters.shape[0])])
+
+from scipy.cluster.hierarchy import dendrogram
+import matplotlib.pyplot as plt
+
+# 검은색 덴드로그램 만들기 (1/2 부분만)
+# from scipy.cluster.hierarchy import set_link_color_palette
+# set_link_color_palette(['black'])
+
+row_dendr = dendrogram(row_clusters,
+                       labels=labels,
+                       # make dendrogram black (part 2/2)
+                       # color_threshold=np.inf
+                       )
+plt.tight_layout()
+plt.ylabel('Euclidean distance')
+# plt.savefig('images/11_11.png', dpi=300,
+#            bbox_inches='tight')
+plt.show()
+
+fig = plt.figure(figsize=(8, 8), facecolor='white')
+axd = fig.add_axes([0.09, 0.1, 0.2, 0.6])
+
+# 노트: matplotlib < v1.5.1일 때는 use orientation='right'를 사용하세요
+row_dendr = dendrogram(row_clusters, orientation='left')
+
+# 군집에 맞게 데이터를 재정렬합니다.
+df_rowclust = df.iloc[row_dendr['leaves'][::-1]]
+
+axd.set_xticks([])
+axd.set_yticks([])
+
+# 덴드로그램의 축을 제거합니다.
+for i in axd.spines.values():
+    i.set_visible(False)
+
+# 히트맵을 출력합니다.
+axm = fig.add_axes([0.23, 0.1, 0.6, 0.6])  # x-위치, y-위치, 너비, 높이
+cax = axm.matshow(df_rowclust, interpolation='nearest', cmap='hot_r')
+fig.colorbar(cax)
+axm.set_xticklabels([''] + list(df_rowclust.columns))
+axm.set_yticklabels([''] + list(df_rowclust.index))
+
+# plt.savefig('images/11_12.png', dpi=300)
+plt.show()
