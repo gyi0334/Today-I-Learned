94b585ca772717c888506e4712eb5235d3265854
지옥혈검 <gyi0334@naver.com>
2025-11-12 22:11:22 +0900

    Update 1111_clustering.py

--- codingtest/2025/1111_clustering.py
+++ codingtest/2025/1111_clustering.py
@@ -1,4 +1,4 @@
-from sklearn.datasets import make_blobs
+"""from sklearn.datasets import make_blobs
 X, y = make_blobs(n_samples=150, n_features=2, centers=3, cluster_std=0.5,
                   shuffle=True, random_state=0)
 
@@ -10,4 +10,23 @@ plt.scatter(X[:,0], X[:,1],c='white', marker='o', edgecolor = 'black', s=50)
 
 from sklearn.cluster import KMeans
 km = KMeans(n_clusters=3, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
-y_km = km.fit_predict(X)
+y_km = km.fit_predict(X)"""
+
+import pandas as pd
+import numpy as np
+
+np.random.seed(123)
+
+variables = ['X', 'Y', 'Z']
+labels = ['ID_0', 'ID_1', 'ID_2', 'ID_3', 'ID_4']
+
+X = np.random.random_sample([5, 3])*10
+df = pd.DataFrame(X, columns=variables, index=labels)
+print(df)
+
+from scipy.spatial.distance import pdist, squareform
+
+row_dist = pd.DataFrame(squareform(pdist(df, metric='euclidean')),
+                        columns=labels,
+                        index=labels)
+print(row_dist)
