e45cc3c83d35be4d49b458fcc1e7a899e55647c9
지옥혈검 <gyi0334@naver.com>
2025-11-19 23:43:53 +0900

    Update 1118_structure of tensorflow.py

--- codingtest/2025/1118_structure of tensorflow.py
+++ codingtest/2025/1118_structure of tensorflow.py
@@ -1,5 +1,23 @@
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
-w = tf.Variable(1.0)
-b = tf.Variable(0.5)
-print(w.trainable, b.trainable)
+tf.random.set_seed(1)
+np.random.seed(1)
+x = np.random.uniform(low=-1, high=1, size=(200, 2))
+y = np.ones(len(x))
+
+print(x.shape) # (200, 2)
+print(y.shape) # (200,)
+
+y[x[:, 0] * x[:, 1]<0] = 0  # 이건 뭐여
+x_train = x[:100, :]
+y_train = y[:100]
+x_valid = x[100:, :]
+y_valid = y[100:]
+fig = plt.figure(figsize=(6,6))
+plt.plot(x[y==0, 0], x[y==0, 1], 'o', alpha=0.75, markersize=10)
+plt.plot(x[y==1, 0], x[y==1, 1], '<', alpha=0.75, markersize=10)
+plt.xlabel(r'$x_1$', size = 15)
+plt.ylabel(r'$x_2$', size = 15)
+plt.show()
