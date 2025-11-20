2a88ebf6e74eb546b42b43a6395be3eea96818f7
지옥혈검 <gyi0334@naver.com>
2025-11-21 00:30:06 +0900

    Update 1120_cnn.py

--- codingtest/2025/1120_cnn.py
+++ codingtest/2025/1120_cnn.py
@@ -6,6 +6,12 @@ def conv1d(x, w, p=0, s=1):
         zero_pad = np.zeros(shape=p)
         x_padded = np.concatenate([zero_pad, x_padded, zero_pad])
     res = []
-    for i in range(0, int((len(x_padded)-len(w_rot))/s+1, s)):
+    for i in range(0, int(len(x_padded)-len(w_rot)/s+1), s):
         res.append(np.sum(x_padded[i:i+w_rot.shape[0]] * w_rot))
     return np.array(res)
+
+x = [1,3,2,4,5,6,1,3]
+w = [1,0,3,1,2]
+
+print('Conv1d implement : ', conv1d(x, w, p=2, s=1))
+print('result of numpy : ',np.convolve(x, w, mode='same'))
