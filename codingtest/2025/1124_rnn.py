7db20faf6647be509b899c4810326a7ef2a1c1c8
지옥혈검 <gyi0334@naver.com>
2025-11-25 22:41:25 +0900

    Update 1124_rnn.py

--- codingtest/2025/1124_rnn.py
+++ codingtest/2025/1124_rnn.py
@@ -25,6 +25,8 @@ ds_raw_train_valid = ds_raw.skip(25000)
 ds_raw_train = ds_raw_train_valid.take(20000)
 ds_raw_valid = ds_raw_train_valid.skip(20000)
 
+print('--------------------------------------')
+
 # STEP 02. 고유 토큰(단어) 찾기
 from collections import Counter
 tokenizer = tfds.deprecated.text.Tokenizer()
@@ -35,7 +37,27 @@ for example in ds_raw_train:
     token_counts.update(tokens)
 print('어휘 사전 크기 : ', len(token_counts))
 
+print('---------------------------------------')
+
 # STEP 03. 고유 토큰을 정수로 인코딩하기
 encoder = tfds.deprecated.text.TokenTextEncoder(token_counts)
 example_str = 'This is an example!'
 print(encoder.encode(example_str))
+
+# STEP 3-A : 변환을 위한 함수 정의
+def encode(text_tensor, label):
+    text = text_tensor.numpy()
+    encoded_text = encoder.encode(text)
+    return encoded_text, label
+
+# STEP 3-B : 함수를 TF 연산으로 변환하기
+def encode_map_fn(text, label):
+    return tf.py_function(encode, inp=[text, label], Tout=(tf.int32, tf.int32))
+ds_train = ds_raw_train.map(encode_map_fn)
+ds_valid = ds_raw_valid.map(encode_map_fn)
+ds_test = ds_raw_test.map(encode_map_fn)
+
+# 샘플의 크기 확인하기:
+tf.random.set_seed(1)
+for example in ds_train.shuffle(1000).take(5):
+    print('시퀀스 길이 : ', example[0].shape)
