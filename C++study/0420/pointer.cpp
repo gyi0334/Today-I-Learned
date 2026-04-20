bc193492d52a8a015b55d30a79f67012534ee84f
지옥혈검 <gyi0334@naver.com>
2026-04-20 23:46:13 +0900

    c++

---
+++ C++study/0420/pointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int updates = 6;  // declare a variable
+    int * p_updates;  // declare pointer to an int
+    p_updates = &updates;
+
+    //express values two ways
+    cout << "Values: updates = " << updates;
+    cout << " , *p_updates = " << *p_updates << endl;
+
+    //express address two ways
+    cout << "Adresses: &updates = " << &updates;
+    cout << ", p_updates = " << p_updates << endl;
+
+    //use pointer to change value
+    *p_updates = *p_updates + 1;
+    cout << "Now updates = " << updates << endl;
+    return 0;
+}
