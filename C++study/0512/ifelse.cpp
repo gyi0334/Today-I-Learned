13d070ff1ea1abbd053cc3a842a3c3f96ee30bfb
지옥혈검 <gyi0334@naver.com>
2026-05-12 23:39:43 +0900

    c++

---
+++ C++study/0512/ifelse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+int main()
+{
+    char ch;
+
+    std::cout << "Type, and I shall repeat.\n";
+    std::cin.get(ch);
+    while (ch != '.')
+    {
+        if (ch == '\n')
+            std::cout << ch;        // done if newline
+        else
+            std::cout << ++ch;      // done otherwise
+        std::cin.get(ch);
+    }
+    // try ch + 1 instead of ++ch for intersting effect
+    std::cout << "\nPlease excuse the slight confusion.\n";
+        // std::cin.get();
+        // std::cin.get();
+    return 0;
+}
