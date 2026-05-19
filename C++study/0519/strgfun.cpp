1c13b779f462f96dcaafea6782702f657a2892ed
지옥혈검 <gyi0334@naver.com>
2026-05-19 23:58:38 +0900

    0519

---
+++ C++study/0519/strgfun.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+unsigned int c_in_str(const char * str, char ch);
+int main()
+{
+    using namespace std;
+    char mmm[15] = "minimum";   // string in an array
+
+    char *wail = "ululate";     // wail points to string
+
+    unsigned int ms = c_in_str(mmm, 'm');
+    unsigned int us = c_in_str(wail, 'u');
+    cout << ms << " m characters in " << mmm << endl;
+    cout << us << " u characters in " << wail << endl;
+    return 0;
+}
+
+unsigned int c_in_str(const char * str, char ch)
+{
+    unsigned int count = 0;
+
+    while (*str)
+    {
+        if (*str == ch)
+            count++;
+        str++;
+    }
+    return count;
+}
