bd4373ddf807b03fba86865710b7b5234b52611a
지옥혈검 <gyi0334@naver.com>
2026-07-09 00:04:04 +0900

    0708

--- opencv_study/test.cpp
+++ opencv_study/test.cpp
@@ -2,45 +2,64 @@
 #include <iostream>
 
 using namespace cv;
+using namespace cv::dnn;
 using namespace std;
 
-void MatOp5()
-{
-    Mat img1 = imread("lenna.bmp");
-
-    cout << "Width: " << img1.cols << endl;
-    cout << "Height: " << img1.rows << endl;
-    cout << "Channels: " << img1.channels() << endl;
-
-    if(img1.type() == CV_8UC1)
-        cout << "img5 is a grayscale image." << endl;
-    else if(img1.type() == CV_8UC3)
-        cout << "img5 is a truecolor image." << endl;
-
-    float data[] = {2.f, 1.414f, 3.f, 1.732f};
-    Mat mat1(2,2, CV_32FC1, data);
-    cout << "mat1:\n" << mat1 << endl;
-}
-
-void MatOp6()
-{
-    float data[] = {1,1,2,3};
-    Mat mat1(2,2,CV_32FC1, data);
-    cout << "mat1:\n" << mat1 << endl;
-
-    Mat mat2 = mat1.inv();
-    cout << "mat2:\n" << mat2 << endl;
-
-    cout << "mat1.t():\n" << mat1.t() << endl;
-    cout << "mat1 + 3:\n" << mat1 + 3 << endl;
-
-    cout << "mat1 + mat2:\n" << mat1 + mat2 << endl;
-    cout << "mat1 * mat2:\n" << mat1 * mat2 << endl;
-}
+void on_mouse(int event, int x, int y, int flags, void* userdata);
 
 int main()
 {
-    MatOp5();
-    MatOp6();
+    Net net = readNet("mnistcnn.onnx");
+
+    if (net.empty()){
+        cerr << "Network load failed!" << endl;
+        return -1;
+    }
+
+    Mat img = Mat::zeros(400, 400, CV_8UC1);            // make black img , size = (400, 400) type = CV_8UC1
+
+    imshow("img", img);                                 // show black img
+    setMouseCallback("img", on_mouse, (void*)&img);     // ?
+
+    while (true){
+        int c = waitKey();
+
+        if (c==27){
+            break;       // key 27 is ESC
+        }
+        else if (c==' '){
+            Mat inputBlob = blobFromImage(img, 1/255.f, Size(28,28));   // nomalization
+            net.setInput(inputBlob);                                    // put inputBlob in the model's input
+            Mat prob = net.forward();                                   // result
+
+            double maxVal;                                              // biget Value
+            Point maxLoc;                                               // bigest location
+            minMaxLoc(prob, NULL, &maxVal, NULL, &maxLoc);              // dont need minval & minloc -> NULL
+            int digit = maxLoc.x;
+
+            cout << digit << " (" << maxVal * 100 << "%)" << endl;
+
+            img.setTo(0);
+            imshow("img", img);
+        }
+    }
     return 0;
+}
+
+Point ptPrev(-1, -1);
+
+void on_mouse(int event, int x, int y, int flags, void* userdata)
+{
+    Mat img = *(Mat*)userdata;
+
+    if (event == EVENT_LBUTTONDOWN){
+        ptPrev = Point(x,y);
+    } else if (event == EVENT_LBUTTONUP){
+        ptPrev = Point(-1,-1);
+    } else if (event == EVENT_MOUSEMOVE && (flags & EVENT_FLAG_LBUTTON)){
+        line(img, ptPrev, Point(x,y), Scalar::all(255), 40, LINE_AA, 0);
+        ptPrev = Point(x,y);
+
+        imshow("img", img);
+    }
 }
